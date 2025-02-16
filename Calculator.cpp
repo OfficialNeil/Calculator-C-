bf99252b3aff975d50e5b8660102a3e180aa0429
Neil Patel <161364682+OfficialNeil@users.noreply.github.com>
2025-02-16 16:55:22 -0500

    On branch Calculator-Changes  Changes to be committed: 	modified:   Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+// Function definitions
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    return a / b;
+}
